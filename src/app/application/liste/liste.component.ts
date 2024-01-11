d9b98304d5d3795f2420301ce41c6233cdfaf660
majoiefaya <majoiefaya@gmail.com>
2024-01-11 14:14:07 +0000

    clean version

--- src/app/application/liste/liste.component.ts
+++ src/app/application/liste/liste.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Caracter } from 'src/app/models/Caracter';
 import { AppService } from 'src/app/services/app.service';
 
@@ -7,55 +7,58 @@ import { AppService } from 'src/app/services/app.service';
   templateUrl: './liste.component.html',
   styleUrls: ['./liste.component.css']
 })
-export class ListeComponent {
+export class ListeComponent implements OnInit {
 
   loading: boolean = false;
   errorMessage: string = '';
   successMessage: string = '';
+  caracters_datas: Caracter[] = [];
   next!: string;
   previous!: string;
   totalPages!: number;
   currentPage = 1;
-  PAGE_SIZE!: number;
+  PAGE_SIZE: number = 20;
+  
 
-  ngOnInit(){
-    let storedPage = Number(localStorage.getItem('currentPage'));
-    if (isNaN(storedPage) || storedPage < 1 || storedPage > this.totalPages) {
-        storedPage = 1;
-    }
-    this.currentPage = storedPage;
+  constructor(private appService: AppService) {}
+
+  ngOnInit() {
+    this.loadStoredPage();
     this.getAllCaracters(this.currentPage);
   }
 
-  caracters_datas:Caracter[]=[]
-  constructor(private appService:AppService
-  ){
+  private loadStoredPage() {
+    let storedPage = Number(localStorage.getItem('currentPage'));
+    if (isNaN(storedPage) || storedPage < 1 || storedPage > this.totalPages) {
+      storedPage = 1;
+    }
+    this.currentPage = storedPage;
   }
-  
-  getAllCaracters(page:number){
+
+  getAllCaracters(page: number): void {
     this.successMessage = '';
-    this.errorMessage='';
+    this.errorMessage = '';
     this.loading = true;
+    
     this.appService.getAll(page).subscribe(
-      (response:any)=>{
-        console.log(response)
-        if (response.results.length===0){
+      (response: any) => {
+        console.log(response);
+        if (response.results.length === 0) {
           // this.notificationService.showError('Erreur',"")
         }
-        this.caracters_datas=response.results
+        this.caracters_datas = response.results;
         this.currentPage = page;
-        this.PAGE_SIZE=20
-        this.totalPages = response.info.pages
+        this.totalPages = response.info.pages;
         this.next = response.info.next;
         this.previous = response.info.prev;
         localStorage.setItem('currentPage', this.currentPage.toString());
         this.loading = false;
       },
-      (error)=>{
-
+      (error) => {
+        // Handle error
       },
-      ()=>{
-        
+      () => {
+        // Optional completion logic
       }
     );
   }
@@ -64,19 +67,23 @@ export class ListeComponent {
     this.currentPage = page;
     localStorage.setItem('currentPage', this.currentPage.toString());
     this.getAllCaracters(this.currentPage);
-}
+  }
 
   nextPage(): void {
     if (this.next) {
-      const nextPage = this.next.split('=')[1]; 
-      this.getAllCaracters(Number(nextPage));
+      const nextPage = this.getPageNumberFromUrl(this.next);
+      this.getAllCaracters(nextPage);
     }
   }
 
   previousPage(): void {
     if (this.previous) {
-      const previousPage = this.previous.split('=')[1]; 
-      this.getAllCaracters(Number(previousPage));
+      const previousPage = this.getPageNumberFromUrl(this.previous);
+      this.getAllCaracters(previousPage);
     }
+  }
+
+  private getPageNumberFromUrl(url: string): number {
+    return Number(url.split('=')[1]);
   }
 }
